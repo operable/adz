d7c7c1e0f39b590e0a66dfaa83c28c75ff79424f
Kevin Smith <kevin@operable.io>
2016-01-26 15:17:12 -0500

    Structured logging & ad-hoc log fields
    * Cleaned up JSON formatting
    * Added zero padding for single digit numbers in timestamps
    * Added support for ad-hoc log fields via the `extra` metadata key

--- lib/adz.ex
+++ lib/adz.ex
@@ -17,18 +17,27 @@ defmodule Adz do
   def text(level, msg, {date, time}, metadata) do
     "#{format_date(date)} #{format_time(time)}"
     <> format_metadata(metadata)
-    <> "[#{level}] #{msg}\n"
+    <> "[#{level}] #{format_message(msg)}"
+    <> format_extra(Keyword.get(metadata, :extra)) <> "\n"
   end
 
   def json(level, msg, {date, time}, metadata) do
-    Poison.encode!(add_metadata(%{date: format_date(date),
-                                  time: format_time(time),
-                                  level: "#{level}",
-                                  message: "#{msg}"}, metadata)) <> "\n"
+    entry = add_metadata(%{timestamp: "#{format_date(date)} #{format_time(time)}",
+                           level: "#{level}",
+                           message: format_message(msg)}, metadata)
+    Poison.encode!(entry) <> "\n"
+  end
+
+  defp format_extra(nil), do: ""
+  defp format_extra(extra), do: "#{inspect extra}"
+
+  defp format_message(msg) when is_binary(msg), do: msg
+  defp format_message(msg) when is_list(msg) do
+    :erlang.iolist_to_binary(msg)
   end
 
   defp format_metadata(metadata) do
-    module = Keyword.get(metadata, :module)
+    module = format_module(Keyword.get(metadata, :module))
     line = Keyword.get(metadata, :line)
     if module != nil and line != nil do
       " (#{module}:#{line}) "
@@ -38,26 +47,77 @@ defmodule Adz do
   end
 
   defp add_metadata(entry, metadata) do
-    module = Keyword.get(metadata, :module)
+    module = format_module(Keyword.get(metadata, :module))
     line = Keyword.get(metadata, :line)
-    if module != nil and line != nil do
+    entry = if module != nil and line != nil do
       entry
-      |> Map.put(:module, "#{module}")
+      |> Map.put(:source, "#{module}")
       |> Map.put(:line, line)
     else
       entry
+      |> Map.put(:source, "n/a")
+      |> Map.put(:line, "n/a")
+    end
+    add_extra(entry, Keyword.get(metadata, :extra))
+  end
+
+  defp add_extra(entry, nil) do
+    entry
+  end
+  defp add_extra(entry, extra) do
+    cond do
+      is_list(extra) ->
+        Map.merge(entry, :maps.from_list(extra), &merge_extra_metadata/3)
+      is_map(extra) ->
+        Map.merge(entry, extra, &merge_extra_metadata/3)
+      true ->
+        Map.put(entry, :extra, "#{inspect extra}")
     end
   end
 
+  defp merge_extra_metadata(_key, v1, v2) do
+    cond do
+      is_list(v1) and is_list(v2) ->
+        v1 ++ v2
+      is_list(v1) ->
+        v1 ++ [v2]
+      is_list(v2) ->
+        [v1|v2]
+      true ->
+        [v1, v2]
+    end
+  end
+
+
+
+  defp format_module(nil), do: nil
+  defp format_module(module) do
+    String.replace("#{module}", ~r/^Elixir\./, "")
+  end
+
   defp format_date({year, month, day}) do
+    month = format_number(month)
+    day = format_number(day)
     "#{month}-#{day}-#{year}"
   end
 
   defp format_time({hour, min, sec}) do
+    hour = format_number(hour)
+    min = format_number(min)
+    sec = format_number(sec)
     "#{hour}:#{min}:#{sec}"
   end
   defp format_time({hour, min, sec, micro}) do
+    hour = format_number(hour)
+    min = format_number(min)
+    sec = format_number(sec)
+    micro = format_number(micro)
     "#{hour}:#{min}:#{sec}:#{micro}"
   end
+
+  defp format_number(n) when n < 10 do
+    "0#{n}"
+  end
+  defp format_number(n), do: "#{n}"
 
 end
