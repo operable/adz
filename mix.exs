5a6431ec19349dd11b1b667616a64dec652e75b7
Kevin Smith <kevin@operable.io>
2016-02-22 16:20:47 -0500

    Updated Elixir vsn requirement

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Adz.Mixfile do
   def project do
     [app: :adz,
      version: "0.2",
-     elixir: "~> 1.1",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
